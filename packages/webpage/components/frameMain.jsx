bcb241f350af50f6b5393bda21cb6aea3955b8b6
github@finnesand.no <github@finnesand.no>
2020-09-14 10:23:34 +0200

    remove website

--- packages/webpage/components/frameMain.jsx
+++
@@ -1,6 +0,0 @@
-import { Box } from 'juhuui';
-import React from 'react'
-
-const FrameMain = (props) => <Box className="frameMain" px="3vw" {...props} />;
-
-export default FrameMain;
--- packages/webpage/components/init.jsx
+++
@@ -1,8 +0,0 @@
-import { init } from '../pages/_app';
-import '../pages/_app';
-
-function test() {
-  this.bla = init.forwardRef;
-}
-
-export default test;
