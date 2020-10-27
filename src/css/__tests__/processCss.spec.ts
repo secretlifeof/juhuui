068413e5e1a49d853cd53bf449b0d46e2bca5d8f
github@finnesand.no <github@finnesand.no>
2020-10-27 17:07:49 +0100

    fix processCss test

--- src/css/__tests__/processCss.spec.ts
+++ src/css/__tests__/processCss.spec.ts
@@ -3,20 +3,18 @@ import { processCss } from '../processCss';
 
 describe('processCSS', () => {
   test('no props, no valid', () => {
-    expect(processCss({}).classNames.length).toBe(0);
+    expect(processCss({ css: {} }).classNames.length).toBe(0);
   });
   test('two props, no valid', () => {
-    const props = { queen: 'yes', kind: 'yes' };
+    const props = { css: { queen: 'yes', kind: 'yes' } };
     expect(processCss(props).classNames.length).toBe(2);
   });
   test('two props, one valid', () => {
-    const props = { color: 'green', kind: 'yes' };
+    const props = { css: { color: 'green', kind: 'yes' } };
     expect(processCss(props).classNames.length).toBe(2);
-    expect(Object.values(processCss(props).returnProps).length).toBe(3);
   });
   test('two props, two valid', () => {
-    const props = { color: 'green', bg: 'black' };
+    const props = { css: { color: 'green', bg: 'black' } };
     expect(processCss(props).classNames.length).toBe(2);
-    expect(Object.values(processCss(props).returnProps).length).toBe(2);
   });
 });
